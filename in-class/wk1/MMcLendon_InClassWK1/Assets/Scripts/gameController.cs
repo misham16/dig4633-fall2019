26a107c0d78dcef6e795f242884cb7ec9f4c6f84
misham16 <mmclendon@knights.ucf.edu>
2019-09-25 22:42:54 -0400

    chickens

---
+++ in-class/wk1/MMcLendon_InClassWK1/Assets/Scripts/gameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public bool chickenHat = false;
+    public GameObject cHat;
+
+    public void ChickenHappy () 
+    {
+        if()
+        {
+            Debug.Log("Chicken is Happy");
+        }
+    }
+
+    void HatRestored()
+    {
+        cHat.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
